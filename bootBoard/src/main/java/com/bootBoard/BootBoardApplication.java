f350313520cd7b46e313463290c023fcdbbe84ea
keh6305 <keh6305@gmail.com>
2022-11-04 23:36:54 +0900

    spring security 추가

--- bootBoard/src/main/java/com/bootBoard/BootBoardApplication.java
+++ bootBoard/src/main/java/com/bootBoard/BootBoardApplication.java
@@ -2,12 +2,14 @@ package com.bootBoard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
-public class BootBoardApplication {
-
-	public static void main(String[] args) {
+public class BootBoardApplication
+{
+	public static void main(String[] args)
+	{
 		SpringApplication.run(BootBoardApplication.class, args);
 	}
-
 }

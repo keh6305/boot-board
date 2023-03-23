4b72290c377edbd9d23e80203015d5b7a3a80c5b
keh6305 <keh6305@gmail.com>
2023-03-23 10:07:41 +0900

    단순 수정

--- bootBoard/src/main/java/com/bootBoard/entity/Step.java
+++ bootBoard/src/main/java/com/bootBoard/entity/Step.java
@@ -10,7 +10,7 @@ import org.apache.ibatis.type.Alias;
 public class Step
 {
     private int step_id;                   // 절차 PK
-    private int recruitment_id;                 // 공고 PK
+    private int recruitment_id;            // 공고 PK
     private String step;                   // 절차
     private String step_comment;           // 절차 설명
     private int step_type;                 // 절차 유형(0: 서류, 1: 테스트, 2: 과제, 3: 면접)
